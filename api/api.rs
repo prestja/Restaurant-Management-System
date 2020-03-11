99a510a1bfa8deb430745f4e48b3c127ca2c24e3
= <prestja@gmail.com>
2020-03-11 21:37:34 +0000

    Adds primitive way of interfacing with MongoDB running on local machine

--- api/api.rs
+++ api/api.rs
@@ -3,11 +3,26 @@
 #[macro_use] extern crate rocket;
 extern crate mongodb;
 
+use mongodb::{Client, options::ClientOptions};
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+fn connect() {
+    println!("Attempting connection");
+    let mut client_options : ClientOptions =
+    ClientOptions::parse("mongodb://localhost:27017").unwrap();
+    client_options.app_name = Some("My App".to_string());
+    let client : Client = Client::with_options(client_options).unwrap();
+
+    for db_name in client.list_database_names(None).unwrap(){
+        println!("{}", db_name);
+    }
+}
+
 fn main() {
+    connect();
     rocket::ignite().mount("/", routes![index]).launch();
 }

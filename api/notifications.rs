8a5322d9d8146c6ce16c4c2e0a6fa81bbe262d30
BaylessL <56482150+BaylessL@users.noreply.github.com>
2020-04-01 22:45:01 -0500

    Update notifications.rs

--- api/notifications.rs
+++ api/notifications.rs
@@ -58,35 +58,35 @@ pub fn get(_conn: LogsDbConn, id: u32) -> String {
 	return str;
 }
 
-#[post("/staff")]
-pub fn post(_conn: LogsDbConn) -> &'static str {
-	let _coll = _conn.collection("staff");
-	_coll.insert_one(doc!{ "empid": 32 }, None).unwrap();
-	return "Inserted an element into database";
-}
+//#[post("/staff")]
+//pub fn post(_conn: LogsDbConn) -> &'static str {
+//	let _coll = _conn.collection("staff");
+//	_coll.insert_one(doc!{ "empid": 32 }, None).unwrap();
+//	return "Inserted an element into database";
+//}
 
-#[post("/staff?<empid>&<tableid>")]
-pub fn update_staff_table(_conn: LogsDbConn, empid: u32, tableid: u32) -> &'static str
-{
-        let _coll = _conn.collection("staff");
-        let filter = doc!{"empid" => empid};
-        let update = doc!{"$set" => {"tableid" => tableid}};
+//#[post("/staff?<empid>&<tableid>")]
+//pub fn update_staff_table(_conn: LogsDbConn, empid: u32, tableid: u32) -> &'static str
+//{
+//        let _coll = _conn.collection("staff");
+//        let filter = doc!{"empid" => empid};
+//        let update = doc!{"$set" => {"tableid" => tableid}};
 
-        _coll.update_one(filter, update, None).unwrap();
-        return "Updated element in database";
-}
+//        _coll.update_one(filter, update, None).unwrap();
+//       return "Updated element in database";
+//}
 
-#[post("/staff/manager?<first>&<last>&<empid>&<position>&<wage>&<phone_num>")]
-pub fn manager_add_staff(_conn: LogsDbConn, first: &rocket::http::RawStr, last: &rocket::http::RawStr, empid: u32, position: u32, wage: f32, phone_num: &rocket::http::RawStr) -> &'static str
-{
-	let _coll = _conn.collection("staff");
-	let doc = doc!{
-	"firstName" => first.as_str(),
-	"lastName" => last.as_str(),
-	"empid" => empid,
-	"position" => position,
-	"wage" => wage,
-	"phone" => phone_num.as_str()};
-        _coll.insert_one(doc, None).unwrap();
-        return "Inserted an element into database";
-} 
+//#[post("/staff/manager?<first>&<last>&<empid>&<position>&<wage>&<phone_num>")]
+//pub fn manager_add_staff(_conn: LogsDbConn, first: &rocket::http::RawStr, last: &rocket::http::RawStr, empid: u32, position: u32, wage: f32, phone_num: &rocket::http::RawStr) -> &'static str
+//{
+//	let _coll = _conn.collection("staff");
+//	let doc = doc!{
+//	"firstName" => first.as_str(),
+//	"lastName" => last.as_str(),
+//	"empid" => empid,
+//	"position" => position,
+//	"wage" => wage,
+//	"phone" => phone_num.as_str()};
+//        _coll.insert_one(doc, None).unwrap();
+//        return "Inserted an element into database";
+//}

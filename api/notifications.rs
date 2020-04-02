fdddf1529fbf95a1f0c8e2eabdeab12536c6bc83
BaylessL <56482150+BaylessL@users.noreply.github.com>
2020-04-02 17:07:21 -0500

    notifications.rs
    Copied from accidental creation in develop, see commit there for full details.

---
+++ api/notifications.rs
@@ -0,0 +1,107 @@
+use crate::rocket_contrib;
+use crate::rocket_contrib::databases::mongodb::db::ThreadedDatabase;
+use crate::LogsDbConn;
+use crate::serde_derive;
+
+use rocket::response::content;
+use rocket_contrib::{databases::mongodb};
+use rocket_contrib::json::Json;
+use mongodb::{doc, bson};
+use mongodb::coll::options;
+use mongodb::oid;
+
+
+//Building a struct to contain notifications within
+#[derive(Serialize, Deserialize)]
+pub struct notification
+{
+	//holds table id from which notif originated
+	table: u32,
+	//Holds type of notification (help, refill, etc)
+	notif_type: u32,
+	//holds details regarding refill or type of help
+	#[serde(default)] details : String
+}
+
+
+//====================================================================================//
+
+//=============================	Get Functions 	===================================//
+//Ready for testing
+//This first get will attempt to grab a copy of all existing notifications in the collection
+#[get("/")]
+pub fn get_all(_conn: LogsDbConn) -> String 
+{
+	//Create a string that we can add more information to as we build our get
+	let mut str = String::from("[\n\t");
+	
+	//Establish connection to db collection for notifications
+	let _coll = _conn.collection("notifications");
+	//Create a scroller that starts at the beginning of our notifications collection
+	let cursor = _coll.find(None, None).unwrap();
+	
+	//For all notifications
+	for result in cursor 
+	{ 
+		//If an item exists
+		if let Ok(item) = result 
+		{
+			//pull the stored item and convert to a json recognizable string (?)
+			let _bson = mongodb::to_bson(&item).unwrap();
+			let _json = serde_json::ser::to_string(&_bson).unwrap();
+			//add the json version of our notification to our message string
+			str.push_str(&_json);
+		}
+		//add terminals between json strings (even final case as we strip terminals before return)
+		str.push_str(",\n\t");
+	}
+	//If cursor was empty str will be of length less than 3
+	if str.len() <= 3
+	{
+		return String::from("No entries found");
+	}
+	
+	//Clean up our message string of any unwanted termimnals
+	str.pop();
+	str.pop();
+	str.pop();
+	str.push_str("\n]");
+	
+	return str;
+}
+
+
+//=============================	Post Functions 	===================================//
+
+// Ready for testing
+//Function that allows for data to be formatted into a json object from a stored notification type object
+#[post("/", data = "<notifications>")]
+pub fn post(_conn: LogsDbConn, notification: Json<Notification>) -> String
+{
+	//doc will be our json message being sent, while inner is what allows us to access the data found in our
+	// notification (?)
+	let inner = notification.into_inner();
+	let doc = doc!
+	{
+		//matches exactly to the notification struct
+		"table": inner.table,
+		"notif_type": inner.notif_type,
+		"details": inner.details
+	};
+	
+	//Establish connection to db collection for notifications
+	let _coll = _conn.collection("notifications");
+	//Attempt to send via connection, the json formatted version of the notification
+	_coll.insert_one(doc, None).unwrap();
+
+	//if post successful
+	let response = json!(
+	{
+		"code": 200,
+		"message": "Inserted notification into notifications collection"
+	});
+	
+	return serde_json::to_string(&response).unwrap();
+	
+}
+//====================================================================================//
